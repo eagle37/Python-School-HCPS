3b33619083b3e3ae1c6f64efb64785e1dc5e3a40
eagle37 <84372934+eagle37@users.noreply.github.com>
2022-05-31 14:58:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,13 +31,23 @@ class Normal:
         Normal.write(f"{Color}> Your Hobby?: {Fore.RESET}")
         Hobby = input()
         Normal.clear()
+        Normal.write(f"{Color}> Your Weight [KG]?: {Fore.RESET}")
+        Weight = input()
+        Normal.clear()
         Normal.write(f"{Color}Getting Results! Wait For 3 Seconds Only!")
         time.sleep(3)
-        Normal.write(f"\n{Color}Here are the results!\nYour Name: {Name}\nYour Age: {Age}\nYour Hobby: {Hobby}\nMade With HardWork By Eagle!\nHope You Liked It!{Fore.RESET}")
+        if int(Weight) > 60 and int(Age) in (14, 15, 16):
         
-        
+            Normal.write(f"\n{Color}Here are the results!\nYour Name: {Name}\nYour Age: {Age}\nYour Hobby: {Hobby}\nYour Weight: {Weight} (You Are Over Weighted According To Your Age!:(){Fore.RESET}\n\n\n{Fore.GREEN}Made With HardWork By Prateek Aggarwal!\nHope You Liked It!{Fore.RESET}")
+
+        elif int(Weight) in (55, 56, 57, 58, 59, 60) and int(Age) in (14, 15, 16):
+            Normal.write(f"\n{Color}Here are the results!\nYour Name: {Name}\nYour Age: {Age}\nYour Hobby: {Hobby}\nYour Weight: {Weight} (You Are Fit According To Your Age! :)){Fore.RESET}\n\n\n{Fore.GREEN}Made With HardWork By Prateek Aggarwal!\nHope You Liked It!{Fore.RESET}")
+        elif int(Weight) < 55 and int(Age) in (14, 15, 16):
+            Normal.write(f"\n{Color}Here are the results!\nYour Name: {Name}\nYour Age: {Age}\nYour Hobby: {Hobby}\nYour Weight: {Weight} (You Are Low Weighted According To Your Age!:(){Fore.RESET}\n\n\n{Fore.GREEN}Made With HardWork By Prateek Aggarwal!\nHope You Liked It!{Fore.RESET}")
+        else:
+            Normal.write(f"\n{Color}Here are the results!\nYour Name: {Name}\nYour Age: {Age}\nYour Hobby: {Hobby}\nYour Weight: {Weight}{Fore.RESET}\n\n\n{Fore.GREEN}Made With HardWork By Prateek Aggarwal!:)\nHope You Liked It!:){Fore.RESET}")
         
         
 if __name__ == "__main__":
         Normal.ask()
-        
+ 
